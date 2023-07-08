63475c3d9c0e6bd4d6a06bfe51015bd686f81b10
Carlin Au <carlinau@gmail.com>
2023-07-08 13:08:28 -0700

    update date formatting and updatesanity api version

--- src/clients/sanityClient.ts
+++ src/clients/sanityClient.ts
@@ -3,5 +3,6 @@ import { createClient } from 'next-sanity';
 export const SanityClient = createClient({
   projectId: '91efs7mg',
   dataset: 'production',
+  apiVersion: 'v2021-10-21',
   useCdn: false,
 });
--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,3 +1,7 @@
 export function formatDate(date: Date): string {
-  return `${date.getMonth()}/${date.getDay()}/${date.getFullYear()} at ${date.getHours()}:${date.getMinutes()}`;
+  return `${
+    date.getMonth() + 1
+  }/${date.getDate()}/${date.getFullYear()} at ${date.getHours()}:${
+    date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()
+  }`;
 }
